38a4297ba5021b95c5e19e2a88a5d17ee8a1a503
BristerMerek <98425427+BristerMerek@users.noreply.github.com>
2022-03-04 18:13:52 -0600

    Create Frequency.v

---
+++ Ethan go ham/Frequency.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/04/2022 05:46:48 PM
+// Design Name: 
+// Module Name: Frequency Signal Detection
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Frequency_Signal_Detection(
+    input LED1
+
+    );
+endmodule
